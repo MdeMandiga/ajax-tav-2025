0a2522fb636f29e18c2966d6ba53ba0a6bed7776
MdeMandiga <150732605+MdeMandiga@users.noreply.github.com>
2025-01-20 09:38:14 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 })
 //funcion ajax
 $.ajax({
-    url: 'texto.txt', // busca la direccion del archivo
+    url: 'assets/js/texto.txt', // busca la direccion del archivo
     type: 'get', // leer equivale a leer el post
     dataType: 'txt', //define el tipo de archivo
     success: function(respuesta){
