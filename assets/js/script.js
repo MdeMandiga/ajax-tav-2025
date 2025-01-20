2c755a1ff937d39a81b3a3703411b67336d72190
MdeMandiga <150732605+MdeMandiga@users.noreply.github.com>
2025-01-20 09:29:50 -0300

    prueba 1 de Ajax

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+})
+//funcion ajax
+$.ajax({
+    url: 'assets/js/texto.txt', // busca la direccion del archivo
+    type: 'get', // leer equivale a leer el post
+    dataType: 'txt', //define el tipo de archivo
+    success: function(respuesta){
+        document.write('respuesta')
+    },
+    error: function(){
+        document.write(error)
+    }
+});
